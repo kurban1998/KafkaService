05115bf34b65a24fea16ce119ee3e314e33b7f95
Kazimagomed <kazi585@mail.ru>
2023-05-31 01:51:31 +0300

    add KafkaSender and KafkaListener

---
+++ KafkaListener/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KafkaListener
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
